f7807b9c4e225b24f0e3cfd0048d34106f790e90
Tejas Parmar <parmart1@roehampton.ac.uk>
2025-11-30 12:34:35 +0000

    Improved the Result class by adding a dedicated constructor and implementing a display method to print results effectively

--- src/PortScanResult.java
+++ src/PortScanResult.java
@@ -1,4 +1,19 @@
 public class PortScanResult {
     int portNumber;
     boolean isOpen;
+
+    // Constructor for the class
+    public PortScanResult(int port, boolean open) {
+        this.portNumber = port;
+        this.isOpen = open;
+    }
+
+    // Prints whether a port is open or not
+    public void display() {
+        if(this.isOpen) {
+            System.out.println("    [+] Port " + this.portNumber + " is OPEN.");
+        } else {
+            System.out.println("    [-] Port " + this.portNumber + " is CLOSED.");
+        }
+    }
 }
--- src/PortScanner.java
+++ src/PortScanner.java
@@ -1,17 +1,10 @@
 public class PortScanner {
     public static void main() {
-        PortScanResult result1 = new PortScanResult();
+        PortScanResult result1 = new PortScanResult(80, true);
+        PortScanResult result2 = new PortScanResult(22, false);
 
-        result1.portNumber = 80;
-        result1.isOpen = true;
-
-        PortScanResult result2 = new PortScanResult();
-
-        result2.portNumber = 22;
-        result2.isOpen = false;
-
-        System.out.println("--- Scan Results ---");
-        System.out.println("Port: " + result1.portNumber + " is open: " + result1.isOpen);
-        System.out.println("Port: " + result2.portNumber + " is open: " + result2.isOpen);
+        System.out.println("Scan Results ---");
+        result1.display();
+        result2.display();
     }
 }
